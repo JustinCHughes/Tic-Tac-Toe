f0bc9fc3540a3ec8751f7b3ef0dba325be7142ec
JustinCHughes <hcjustin713@gmail.com>
2024-09-17 22:40:17 -0500

    Added logic to check if user input was into an empty cell

--- src/main/java/com/csc/TicTacToe.java
+++ src/main/java/com/csc/TicTacToe.java
@@ -5,23 +5,27 @@ public class TicTacToe
     char[][] board = {{' ', ' ', ' '},
 	    								{' ', ' ', ' '},
 											{' ', ' ', ' '}};
-    boolean victory = false;
+    String currentState = "Continue";
     int currentPlayer = 1;
 
-    while(victory = false)
+    while(currentState.equals("Continue"))
     {
       printBoard(board);
-    
-      victory = gameState(board,currentPlayer);
+   		
+			playerSelect(board, currentPlayer);
 
-      if(currentPlayer == 2)
-      {
-        currentPlayer = 1;
-      }
-      else
-      {
-        currentPlayer = 2;
-      }
+      currentState = gameState(board,currentPlayer);
+      if(currentState.equals("Continue")
+			{
+        if(currentPlayer == 2)
+        {
+          currentPlayer = 1;
+        }
+        else
+        {
+          currentPlayer = 2;
+        }
+			}
     }
   }
 
@@ -42,50 +46,120 @@ public class TicTacToe
     {
       switch playerSelection:
         case '1':
-    	  	char[0][0] = 'X';
+					board[0][0] = 'X';
         case '2':
-    	  	char[0][1] = 'X';
+    	  	board[0][1] = 'X';
         case '3':
-    	  	char[0][2] = 'X';
+    	  	board[0][2] = 'X';
         case '4':
-    	  	char[1][0] = 'X';
+    	  	board[1][0] = 'X';
         case '5':
-    	  	char[1][1] = 'X';
+    	  	board[1][1] = 'X';
         case '6':
-    	  	char[1][2] = 'X';
+    	  	board[1][2] = 'X';
         case '7':
-    	  	char[2][0] = 'X';
+    	  	board[2][0] = 'X';
         case '8':
-    	  	char[2][1] = 'X';
+    	  	board[2][1] = 'X';
         case '9':
-    	  	char[2][2] = 'X';
+    	  	board[2][2] = 'X';
     }
     else
     {
       switch playerSelection:
         case '1':
-    	  	char[0][0] = 'O';
+    	  	board[0][0] = 'O';
         case '2':
-    	  	char[0][1] = 'O';
+    	  	board[0][1] = 'O';
         case '3':
-    	  	char[0][2] = 'O';
+    	  	board[0][2] = 'O';
         case '4':
-    	  	char[1][0] = 'O';
+    	  	board[1][0] = 'O';
         case '5':
-    	  	char[1][1] = 'O';
+    	  	board[1][1] = 'O';
         case '6':
-    	  	char[1][2] = 'O';
+    	  	board[1][2] = 'O';
         case '7':
-    	  	char[2][0] = 'O';
+    	  	board[2][0] = 'O';
         case '8':
-    	  	char[2][1] = 'O';
+    	  	board[2][1] = 'O';
         case '9':
-    	  	char[2][2] = 'O';
+    	  	board[2][2] = 'O';
     }
+		return board;
   }
 
+	// Check if selected cell is empty
+	public static boolean cellEmpty(char[][] board, int playerSelection)
+	{
+		switch playerSelection:
+			case 1:
+				if(board[0][0] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 2:
+				if(board[0][1] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 3:
+				if(board[0][2] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 4:
+				if(board[1][0] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 5:
+				if(board[1][1] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 6:
+				if(board[1][2] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 7:
+				if(board[2][0] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 8:
+				if(board[2][1] == ' ')
+				{
+					return true;
+				}
+				break;
+			case 9:
+				if(board[2][2] == ' ')
+				{
+					return true;
+				}
+				break;
+		return false;
+	}
+
+	// Ask for input
+	public static void playerSelect(char[][] board, int currentPlayer)
+	{
+		Scanner in = new Scanner();
+
+		
+	}
+
   // Check game state
-  public static boolean gameState(char[][] board, int currentPlayer)
+  public static String gameState(char[][] board, int currentPlayer)
   {
     if((board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
        (board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
@@ -96,12 +170,23 @@ public class TicTacToe
        (board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
        (board[2][0] == board[1][1] && board[1][1] == board[0][2]))
     {
-      return true;
+      return "Victory";
     }
-    else if(
+    else if(' ' != board[0][0] &&
+            ' ' != board[0][1] &&
+            ' ' != board[0][2] &&
+            ' ' != board[1][0] &&
+            ' ' != board[1][1] &&
+            ' ' != board[1][2] &&
+            ' ' != board[2][0] &&
+            ' ' != board[2][1] &&
+            ' ' != board[2][2])
+    {
+			return "Draw";
+		}
     else
     {
-      return false;
+      return "Continue";
     }
   }
 }
