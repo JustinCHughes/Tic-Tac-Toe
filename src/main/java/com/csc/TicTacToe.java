201491b1eb3ebf8a1edc79f3fa822e57113c8878
JustinCHughes <hcjustin713@gmail.com>
2024-09-18 17:32:16 -0500

    Completed all tic tac toe game logic

--- src/main/java/com/csc/TicTacToe.java
+++ src/main/java/com/csc/TicTacToe.java
@@ -1,98 +1,134 @@
+import java.util.Scanner;
+
 public class TicTacToe
 {
-  public static void main(String[] args)
-  {
-    char[][] board = {{' ', ' ', ' '},
-	    								{' ', ' ', ' '},
-											{' ', ' ', ' '}};
-    String currentState = "Continue";
-    int currentPlayer = 1;
+	public static void main(String[] args)
+	{
+		char[][] board = {{' ', ' ', ' '},
+	    					{' ', ' ', ' '},
+							{' ', ' ', ' '}};
+		String currentState = "Continue";
+		int currentPlayer = 1;
 
-    while(currentState.equals("Continue"))
-    {
-      printBoard(board);
-   		
+		while(currentState.equals("Continue"))
+		{
+			System.out.flush();
+			printBoard(board);
 			playerSelect(board, currentPlayer);
 
-      currentState = gameState(board,currentPlayer);
-      if(currentState.equals("Continue")
+			currentState = gameState(board,currentPlayer);
+			if(currentState.equals("Continue"))
 			{
-        if(currentPlayer == 2)
-        {
-          currentPlayer = 1;
-        }
-        else
-        {
-          currentPlayer = 2;
-        }
+				if(currentPlayer == 2)
+				{
+					currentPlayer = 1;
+				}
+				else
+				{
+					currentPlayer = 2;
+				}
 			}
-    }
-  }
+		}
+
+		printBoard(board);
+
+		if(currentState.equals("Victory"))
+		{
+			System.out.println("Player " + Integer.toString(currentPlayer) + " has won!");
+		}
+		else
+		{
+			System.out.println("The game has ended in a draw!");
+		}
+	}
 
   // Prints current state Tic Tac Toe board
-  public static void printBoard(char[][] board)
-  {
-    System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
-    System.out.println("-----------");
-    System.out.println(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
-    System.out.println("-----------");
-    System.out.println(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
-  }
+	public static void printBoard(char[][] board)
+	{
+		System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+		System.out.println("-----------");
+		System.out.println(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+		System.out.println("-----------");
+		System.out.println(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+	}
 
-  // Update board state depending on user selection
-  public static void playerSelect(char[][] board, int playerSelection, int currentPlayer)
-  {
-    if(currentPlayer = 1)
-    {
-      switch playerSelection:
-        case '1':
+	// Update board state depending on user selection
+	public static char[][] playerSelect(char[][] board, int playerSelection, int currentPlayer)
+	{
+		if(currentPlayer == 1)
+		{
+			switch(playerSelection)
+			{
+				case 1:
 					board[0][0] = 'X';
-        case '2':
-    	  	board[0][1] = 'X';
-        case '3':
-    	  	board[0][2] = 'X';
-        case '4':
-    	  	board[1][0] = 'X';
-        case '5':
-    	  	board[1][1] = 'X';
-        case '6':
-    	  	board[1][2] = 'X';
-        case '7':
-    	  	board[2][0] = 'X';
-        case '8':
-    	  	board[2][1] = 'X';
-        case '9':
-    	  	board[2][2] = 'X';
-    }
-    else
-    {
-      switch playerSelection:
-        case '1':
-    	  	board[0][0] = 'O';
-        case '2':
-    	  	board[0][1] = 'O';
-        case '3':
-    	  	board[0][2] = 'O';
-        case '4':
-    	  	board[1][0] = 'O';
-        case '5':
-    	  	board[1][1] = 'O';
-        case '6':
-    	  	board[1][2] = 'O';
-        case '7':
-    	  	board[2][0] = 'O';
-        case '8':
-    	  	board[2][1] = 'O';
-        case '9':
-    	  	board[2][2] = 'O';
-    }
+					break;
+				case 2:
+					board[0][1] = 'X';
+					break;
+				case 3:
+					board[0][2] = 'X';
+					break;
+				case 4:
+					board[1][0] = 'X';
+					break;
+				case 5:
+					board[1][1] = 'X';
+					break;
+				case 6:
+					board[1][2] = 'X';
+					break;
+				case 7:
+					board[2][0] = 'X';
+					break;
+				case 8:
+					board[2][1] = 'X';
+					break;
+				case 9:
+					board[2][2] = 'X';
+					break;
+			}
+		}
+		else
+		{
+			switch(playerSelection)
+			{
+				case 1:
+					board[0][0] = 'O';
+					break;
+				case 2:
+					board[0][1] = 'O';
+					break;
+				case 3:
+					board[0][2] = 'O';
+					break;
+				case 4:
+					board[1][0] = 'O';
+					break;
+				case 5:
+					board[1][1] = 'O';
+					break;
+				case 6:
+					board[1][2] = 'O';
+					break;
+				case 7:
+					board[2][0] = 'O';
+					break;
+				case 8:
+					board[2][1] = 'O';
+					break;
+				case 9:
+					board[2][2] = 'O';
+					break;
+			}
+		}
 		return board;
-  }
+	}
 
 	// Check if selected cell is empty
 	public static boolean cellEmpty(char[][] board, int playerSelection)
 	{
-		switch playerSelection:
+		switch(playerSelection)
+		{
 			case 1:
 				if(board[0][0] == ' ')
 				{
@@ -147,46 +183,81 @@ public class TicTacToe
 					return true;
 				}
 				break;
+		}
 		return false;
 	}
 
 	// Ask for input
-	public static void playerSelect(char[][] board, int currentPlayer)
+	public static char[][] playerSelect(char[][] board, int currentPlayer)
 	{
-		Scanner in = new Scanner();
-
+		Scanner in = new Scanner(System.in);
 		
+		boolean valid = false;
+		int selection;
+
+		do
+		{
+			System.out.println("Please select an empty square from 1-9");
+			if(in.hasNextInt())
+			{
+				selection = in.nextInt();
+				if(selection > 0 && selection < 10)
+				{
+					if(cellEmpty(board,selection))
+					{
+						board = playerSelect(board, selection, currentPlayer);
+						valid = true;
+					}
+					else
+					{
+						System.out.println("That cell is taken. Please select a different cell\n");
+					}
+				}
+				else
+				{
+					System.out.println("That is not a valid cell. Please select an empty square from 1-9\n");
+				}
+			}
+			else
+			{
+				System.out.println("That is not an integer. Please try again\n");
+			}
+			in.nextLine();
+
+		} while(!valid);
+		
+		return board;
 	}
 
-  // Check game state
-  public static String gameState(char[][] board, int currentPlayer)
-  {
-    if((board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
-       (board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
-       (board[2][0] == board[2][1] && board[2][1] == board[2][2]) ||
-       (board[0][0] == board[1][0] && board[1][0] == board[2][0]) ||
-       (board[0][1] == board[1][1] && board[1][1] == board[2][1]) ||
-       (board[0][2] == board[1][2] && board[1][2] == board[2][2]) ||
-       (board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-       (board[2][0] == board[1][1] && board[1][1] == board[0][2]))
-    {
-      return "Victory";
-    }
-    else if(' ' != board[0][0] &&
-            ' ' != board[0][1] &&
-            ' ' != board[0][2] &&
-            ' ' != board[1][0] &&
-            ' ' != board[1][1] &&
-            ' ' != board[1][2] &&
-            ' ' != board[2][0] &&
-            ' ' != board[2][1] &&
-            ' ' != board[2][2])
-    {
+	// Check game state
+	public static String gameState(char[][] board, int currentPlayer)
+	{
+		if((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][0] != ' ' ||
+			(board[1][0] == board[1][1] && board[1][1] == board[1][2]) && board[1][0] != ' ' ||
+			(board[2][0] == board[2][1] && board[2][1] == board[2][2]) && board[2][0] != ' ' ||
+			(board[0][0] == board[1][0] && board[1][0] == board[2][0]) && board[0][0] != ' ' ||
+			(board[0][1] == board[1][1] && board[1][1] == board[2][1]) && board[0][1] != ' ' ||
+			(board[0][2] == board[1][2] && board[1][2] == board[2][2]) && board[0][2] != ' ' ||
+			(board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[0][0] != ' ' ||
+			(board[2][0] == board[1][1] && board[1][1] == board[0][2]) && board[2][0] != ' ')
+		{
+			return "Victory";
+		}
+		else if(' ' != board[0][0] &&
+				' ' != board[0][1] &&
+				' ' != board[0][2] &&
+				' ' != board[1][0] &&
+				' ' != board[1][1] &&
+				' ' != board[1][2] &&
+				' ' != board[2][0] &&
+				' ' != board[2][1] &&
+				' ' != board[2][2])
+		{
 			return "Draw";
 		}
-    else
-    {
-      return "Continue";
-    }
-  }
+		else
+		{
+			return "Continue";
+		}
+	}
 }
