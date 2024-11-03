3cde50ed6783f923aa1fb1685dbaa9da410c4e3e
JustinCHughes <hcjustin713@gmail.com>
2024-11-02 20:24:50 -0500

    Added ending menu logic and the options to play again or close Tic Tac Toe

--- src/main/java/com/csc/TicTacToe.java
+++ src/main/java/com/csc/TicTacToe.java
@@ -1,24 +1,62 @@
 package com.csc;
 
+import java.util.Scanner;
+
 public class TicTacToe
 {
 	public static void main(String[] args)
 	{
-		// Initialize variables
-		char[][] board = {{'1', '2', '3'},
-                      {'4', '5', '6'},
-                      {'7', '8', '9'}};
-    String currentState = "Continue";
-
+		int redo = 0;
 		int humanComputer = ModeSelection.player();
 
-		if (humanComputer == 1)
+		do
 		{
-			Human.startGame(board, currentState);
-		}
-		else
-		{
-			Computer.startGame(board, currentState);
-		}
+			// Initialize variables
+			char[][] board = {{'1', '2', '3'},
+												{'4', '5', '6'},
+												{'7', '8', '9'}};
+			String currentState = "Continue";
+
+			if (humanComputer == 1)
+			{
+				Human.startGame(board, currentState);
+			}
+			else
+			{
+				Computer.startGame(board, currentState);
+			}
+
+			do
+			{
+				System.out.println("Would you like to play again?\n\n");
+				System.out.println("(1) Yes\n");
+				System.out.println("(2) No\n\n");
+
+				Scanner in = new Scanner(System.in);
+
+				if(in.hasNextInt())
+				{
+					redo = in.nextInt();
+					if(redo == 1)
+					{
+						System.out.println("You have chosen to play another game!");
+						break;
+					}
+					if(redo == 2)
+					{
+						System.out.println("You have chosen to quit. Goodbye!");
+						break;
+					}
+					else
+					{
+						System.out.println("That is not a valid input. Please choose either 1 or 2\n");
+					}
+				}
+				else
+				{
+					System.out.println("That is not an integer. Please input integers 1 or 2");
+				}
+			} while(true);
+		} while(redo != 2);
 	}
 }
